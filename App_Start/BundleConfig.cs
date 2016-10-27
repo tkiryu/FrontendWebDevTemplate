efdcecaae78dc75db42f7f34b2d4067a042029bd
tkiryu <mr.mistyrain@gmail.com>
2016-10-28 02:39:28 +0900

    upload all source code to repository

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace $safeprojectname$
+{
+    public class BundleConfig
+    {
+        // バンドルの詳細については、http://go.microsoft.com/fwlink/?LinkId=301862  を参照してください
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            BundleTable.EnableOptimizations = true;
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/node_modules/jquery/dist/jquery.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/node_modules/jquery-validation/dist/jquery.validate.js",
+                        "~/node_modules/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js"));
+
+            // 開発と学習には、Modernizr の開発バージョンを使用します。次に、実稼働の準備が
+            // できたら、http://modernizr.com にあるビルド ツールを使用して、必要なテストのみを選択します。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/node_modules/bootstrap/dist/js/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/node_modules/bootstrap/dist/css/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
